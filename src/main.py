793841ecfaaf34b9df48cfd3de1b05d02456db3b
Rigel Di Scala <rigel.discala@propylon.com>
2016-10-15 21:57:04 +0100

    Refactor the timer

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.button import Button
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -34,31 +34,46 @@ class Time(int):
 class TimerButton(Button):
     """The main clickable button.
     """
-    default_round_duration = 180
+    default_round_duration = 15
     formatted_time = StringProperty()
+    clock_state = BooleanProperty()
 
     def __init__(self):
         super(TimerButton, self).__init__()
-        self.time = Time(self.default_round_duration)
+        self._init_round_time()
         self.clock_state = states.PAUSED
-        self._text_state = 0
-        Clock.schedule_interval(self.update, 1)
+        Clock.schedule_interval(lambda event: self.update(), 1)
 
     def _update_formatted_time(self):
-        self.formatted_time = str(self._time)
+        self.formatted_time = str(self._round_time)
 
     @property
-    def time(self):
-        return self._time
+    def round_time(self):
+        return self._round_time
 
-    @time.setter
-    def time(self, value):
-        self._time = value
+    @round_time.setter
+    def round_time(self, value):
+        self._round_time = value
         self._update_formatted_time()
 
-    def update(self, *args):
+    def on_clock_state(self, widget, value):
+        if value:
+            self.background_color = 0, 0, 0, 0
+        else:
+            self.background_color = 1, 1, 1, 1
+
+    def _init_round_time(self):
+        self.round_time = Time(self.default_round_duration)
+
+    def update(self):
         if self.clock_state == states.RUNNING:
-            self.time -= 1
+            if self.round_time > 0:
+                self.round_time -= 1
+            else:
+                self._init_round_time()
+
+    def toggle(self):
+        self.clock_state ^= 1
 
 
 class BoxingApp(App):
