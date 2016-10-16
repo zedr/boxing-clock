4e0b70e21dc68204fd27ebc170834032dbba53b0
Rigel Di Scala <rigel.discala@propylon.com>
2016-10-16 15:08:12 +0100

    Refactor round_time to use a Kivy property

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from collections import namedtuple
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.button import Button
-from kivy.properties import StringProperty, BooleanProperty
+from kivy.properties import StringProperty, BooleanProperty, NumericProperty
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -12,31 +12,17 @@ logging.basicConfig(level=logging.INFO)
 states = namedtuple("States", ("PAUSED", "RUNNING"))(*range(2))
 
 
-class Time(int):
-    @property
-    def minutes(self):
-        return self // 60
-
-    @property
-    def seconds(self):
-        return self % 60
-
-    def __str__(self):
-        return "{0}:{1:02d}".format(self.minutes, self.seconds)
-
-    def __isub__(self, other):
-        return Time(self - other)
-
-    def __iadd__(self, other):
-        return Time(self + other)
+def format_time(seconds):
+    return "{0}:{1:02d}".format(seconds // 60, seconds % 60)
 
 
 class TimerButton(Button):
     """The main clickable button.
     """
-    default_round_duration = 15
+    default_round_duration = 180
     formatted_time = StringProperty()
     clock_state = BooleanProperty()
+    round_time = NumericProperty()
 
     def __init__(self):
         super(TimerButton, self).__init__()
@@ -44,26 +30,17 @@ class TimerButton(Button):
         self.clock_state = states.PAUSED
         Clock.schedule_interval(lambda event: self.update(), 1)
 
-    def _update_formatted_time(self):
-        self.formatted_time = str(self._round_time)
+    def _init_round_time(self):
+        self.round_time = self.default_round_duration
 
-    @property
-    def round_time(self):
-        return self._round_time
-
-    @round_time.setter
-    def round_time(self, value):
-        self._round_time = value
-        self._update_formatted_time()
+    def on_round_time(self, widget, value):
+        self.formatted_time = format_time(value)
 
     def on_clock_state(self, widget, value):
         if value:
             self.background_color = 0, 0, 0, 0
         else:
             self.background_color = 1, 1, 1, 1
-
-    def _init_round_time(self):
-        self.round_time = Time(self.default_round_duration)
 
     def update(self):
         if self.clock_state == states.RUNNING:
