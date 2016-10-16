1d73377b1235ac9dfcf930240361609c91ea761c
Rigel Di Scala <rigel.discala@propylon.com>
2016-10-16 21:02:18 +0100

    Implement the cooldown period

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+class States(object):
+    PAUSED = 0
+    RUNNING = 1
+
+
+class Colors(object):
+    WHITE = "fff"
+    RED = "f33"
+    YELLOW = "ff3"
+    GREEN = "3f3"
+    CYAN = "ccf"
--- src/main.py
+++ src/main.py
@@ -1,46 +1,60 @@
 import logging
-from collections import namedtuple
 
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.button import Button
 from kivy.properties import StringProperty, BooleanProperty, NumericProperty
 
+from constants import Colors, States
+
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-states = namedtuple("States", ("PAUSED", "RUNNING"))(*range(2))
 
-
-def format_time(seconds):
-    return "{0}:{1:02d}".format(seconds // 60, seconds % 60)
+def format_time(seconds, color=Colors.WHITE):
+    return "[color={0}]{1}:{2:02d}[/color]".format(color, seconds // 60,
+                                                   seconds % 60)
 
 
 class TimerButton(Button):
     """The main clickable button.
     """
-    default_round_duration = 180
+    markup = True
+    default_round_duration = 15
     default_warmup_duration = 10
+    default_cooldown_duration = 30
     formatted_time = StringProperty()
     clock_state = BooleanProperty()
     round_time = NumericProperty()
     warmup_time = NumericProperty()
+    cooldown_time = NumericProperty()
 
     def __init__(self):
         super(TimerButton, self).__init__()
         self._init_time()
-        self.clock_state = states.PAUSED
+        self.clock_state = States.PAUSED
         Clock.schedule_interval(lambda event: self.update(), 1)
 
-    def _init_time(self):
+    def _init_time(self, include_warmup=True):
+        if include_warmup:
+            self.warmup_time = self.default_warmup_duration
+        self.cooldown_time = self.default_cooldown_duration
         self.round_time = self.default_round_duration
-        self.warmup_time = self.default_warmup_duration
 
     def on_round_time(self, widget, value):
-        self.formatted_time = format_time(value)
+        self.formatted_time = format_time(
+            value,
+            color=Colors.WHITE if value > 10 else Colors.RED
+        )
 
     def on_warmup_time(self, widget, value):
-        self.formatted_time = format_time(value)
+        self.formatted_time = format_time(value, color=Colors.YELLOW)
+
+    def on_cooldown_time(self, widget, value):
+        self.formatted_time = format_time(
+            value,
+            color=Colors.GREEN if value > 10 else Colors.YELLOW
+        )
 
     def on_clock_state(self, widget, value):
         if value:
@@ -49,14 +63,15 @@ class TimerButton(Button):
             self.background_color = 1, 1, 1, 1
 
     def update(self):
-        if self.clock_state == states.RUNNING:
+        if self.clock_state == States.RUNNING:
             if self.warmup_time:
                 self.warmup_time -= 1
             elif self.round_time:
                 self.round_time -= 1
+            elif self.cooldown_time:
+                self.cooldown_time -= 1
             else:
-                self._init_time()
-
+                self._init_time(include_warmup=False)
 
     def toggle(self):
         self.clock_state ^= 1
