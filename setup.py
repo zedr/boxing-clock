4577c2dc7d351dd6d112cb5cce62455f7bc3db03
Rigel Di Scala <rigel.discala@propylon.com>
2016-10-29 21:07:42 +0100

    Fix the setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Cerebellum setup file.
+"""Boxing clock setup file.
 """
 
 # Always prefer setuptools over distutils
@@ -14,10 +14,10 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='cerebellum',
-    version='0.0.1',
+    name='boxe_clock',
+    version='1.0.0',
 
-    description='A puzzle game from the microcomputer era',
+    description='A simple boxing timer for mobile devices',
     long_description=long_description,
 
     url='',
@@ -31,7 +31,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Web',
+        'Topic :: Other/Nonlisted Topic',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
@@ -45,23 +45,4 @@ setup(
     package_dir={'': 'src'},
 
     install_requires=['kivy==1.9.1', 'cython', 'jnius'],
-
-    # package_data={
-    #    'sample': ['package_data.dat'],
-    # },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-       'console_scripts': [
-           'cerebellum-cli=cerebellum.apps.terminal:main',
-       ],
-    },
 )
